9c8d3d17ece7bf2efbd7b0b2b350f8739312d18b
Audrey Wong <audreydotwong@gmail.com>
2021-06-16 07:55:17 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
 
       <main>
 
-        <Header title="Welcome to my awesome app!" />
+        <Header title="Welcome to littlebannerlady.com" />
     
         <p className="description">
           Get started by editing <code>pages/index.js</code>
